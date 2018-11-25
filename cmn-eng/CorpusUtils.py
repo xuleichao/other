74bb1f5d72cd58ab3429d750e43cb46d123e6187
chao_xlc <1205448156@qq.com>
2018-11-25 19:33:43 +0800

    Add files via upload

---
+++ cmn-eng/CorpusUtils.py
@@ -0,0 +1,175 @@
+'''
+
+by xlc time:2018-08-28 08:27:06
+'''
+import sys
+#sys.path.append('D:/svn_codes/source/public_fun')
+import os
+main_path = os.path.dirname(os.path.abspath(__file__)).replace('\\', '/')
+main_path = '/'.join(main_path.split('/')[:-1])
+import pandas as pd
+import numpy as np
+import jieba, copy
+from sklearn.feature_extraction.text import CountVectorizer
+
+class CorpusUtils:
+    """
+    ## 语料数据处理
+    1. 处理语料处理为one_hot
+    2. 字符到数字的字典
+    3. 数字到字符的字典
+    """
+
+    def __init__(self, corpus_data):
+        self.raw_data = corpus_data # 原始数据
+        self.words_num = len(self.raw_data)
+        self.add_special()
+        self.vocabulary_reverse()
+        self.gnrt_onehot_dct()
+        
+        
+    def add_special(self):
+        # add `bos` `pad` `eos` `unk`
+        self.raw_data['bos'] = self.words_num + 0
+        self.raw_data['pad'] = self.words_num + 1
+        self.raw_data['eos'] = self.words_num + 2
+        self.raw_data['unk'] = self.words_num + 3
+    
+    def corpus_one_hot(self, a_number):
+        
+        zero_matrix = np.zeros((self.words_num + 4))
+        zero_matrix[a_number] = 1
+        return zero_matrix
+
+    def gnrt_onehot_dct(self):
+        onehot_dct = {}
+        for k, v in self.raw_data.items():
+            onehot_dct[k] = self.corpus_one_hot(v)
+        self.onehot_dct = onehot_dct
+        return onehot_dct
+
+    def vocabulary_reverse(self):
+        reverse_dct = {v: k for k, v in self.raw_data.items()}
+        self.reverse_dct = reverse_dct
+    
+    def return_data(self):
+        return self.raw_data, self.reverse_dct
+
+def data_utils(corpus_num=100):
+
+    def _del_space(lst):
+        # 删除英文空格
+        lst = [[j for j in i if j!=' '] for i in lst]
+        return lst
+        
+    data_path = './cmn-eng/cmn.txt'
+    data = pd.read_csv(data_path, sep='\t', names=['eng', 'chi'])[: corpus_num]
+    eng_tokenize_lst = []
+    chi_tokenize_lst = []
+    for i in range(corpus_num):
+        eng_data = data.iloc[i]['eng']
+        chi_data = data.iloc[i]['chi']
+        eng_token_lst = jieba.lcut(eng_data)
+        chi_token_lst = jieba.lcut(chi_data)
+        eng_tokenize_lst.append(eng_token_lst)
+        chi_tokenize_lst.append(chi_token_lst)
+    #eng_tokenize_lst = _del_space(eng_tokenize_lst)
+    return eng_tokenize_lst, chi_tokenize_lst
+        
+def get_data(language='eng'):
+    data_path = './cmn-eng/cmn.txt'
+    data = pd.read_csv(data_path, sep='\t', names=['eng', 'chi'])[:100]
+    tokenize_lst = []
+    for i in range(data.shape[0]):
+        eng_data = data.iloc[i][language]
+        #chi_data = data.iloc[i]['chi']
+        eng_token_lst = jieba.lcut(eng_data)
+        #chi_token_lst = jieba.lcut(chi_data)
+        tokenize_lst += eng_token_lst
+        #tokenize_lst += chi_token_lst
+    #counter = CountVectorizer()
+    #counter.fit_transform(tokenize_lst)
+    #vocabulary = counter.vocabulary_
+    #words_num = len(vocabulary)
+    #print(vocabulary)
+    words_set = list(set(tokenize_lst))
+    print(len(words_set))
+    vocabulary = {}
+    for i in range(len(words_set)):
+        vocabulary[words_set[i]] = i
+    mycls = CorpusUtils(vocabulary)
+    rdata = mycls.return_data()
+    return rdata
+
+def sents2onehot(sents_lst, dictionary):
+    new_sents_lst = []
+    for i in sents_lst:
+        vec = dictionary.get(i)
+        if vec is None:
+            vec = dictionary.get('unk')
+        new_sents_lst.append(vec)
+    return new_sents_lst
+
+def gnrt_learn_data(language='eng'):
+    X, y = data_utils() #
+    str2onehot_dict, _ = get_data(language)
+    str2onehot_dict_chi, _ = get_data('chi')
+    #print(X)
+    #print(str2onehot_dict)
+    X = [sents2onehot(i, str2onehot_dict) for i in X]
+    y = [sents2onehot(i, str2onehot_dict_chi) for i in y]
+    return X, y, str2onehot_dict, str2onehot_dict_chi
+    
+def data_padding(inputs_lst, max_padding, dictionary):
+    new_inputs_lst = []
+    for i in inputs_lst:
+        if len(i) >= max_padding:
+            new_inputs_lst.append(i[: max_padding])
+        else:
+            for j in range(abs(len(i)-max_padding)):
+                i += [dictionary['pad']]
+            new_inputs_lst.append(i)
+    return new_inputs_lst
+
+def target_utils(target, max_padding, dictionary, is_target=False):
+    new_target = []
+    if is_target == False:
+        for i in target:
+            i.insert(0, dictionary['bos'])
+    if is_target == True:
+        for i in target:
+            i.append(dictionary['eos'])
+    for i in target:
+        if len(i) >= max_padding:
+            new_target.append(i[: max_padding])
+        else:
+            for j in range(abs(len(i)-max_padding)):
+                i += [dictionary['pad']]
+            new_target.append(i)
+    #if is_target == False:
+    #    for i in target:
+    #        i.append(dictionary['eos'])
+    if is_target == True:
+        decoder_target = [i[: -1] for i in new_target]
+        return decoder_target
+    else:
+        decoder_input = [i[: -1] for i in new_target]
+        return decoder_input
+    return decoder_target, decoder_input
+
+def batch_data(data_tuple, batch_size):
+    # 将 dataflow_loads 的返回值变为batch
+    batch_rslt = []
+
+def dataflow_loads(language='eng'):
+    X, y, dct, dct_chi = gnrt_learn_data(language)
+    y1 =copy.deepcopy(y)
+    X = data_padding(X, 15, dct)
+    decoder_target = target_utils(y, 20, dct_chi, True)
+    decoder_input = target_utils(y1, 20, dct_chi, False)
+    return X, decoder_target, decoder_input
+    
+if __name__ == '__main__':
+    X, decoder_target, decoder_input = dataflow_loads()
+    
+        
---
+++ cmn-eng/model_build.py
@@ -0,0 +1,171 @@
+'''
+
+by xlc time:2018-08-23 09:50:18
+'''
+import sys
+#sys.path.append('D:/svn_codes/source/public_fun')
+import os
+main_path = os.path.dirname(os.path.abspath(__file__)).replace('\\', '/')
+main_path = '/'.join(main_path.split('/')[:-1])
+from CorpusUtils import get_data, dataflow_loads
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+dictionary_eng = get_data()
+dictionary_chi = get_data('chi')
+#encoder_inputs, decoder_target, decoder_input = dataflow_loads()
+
+def build_embedding(units_num, embedding_length, name='embedding'):
+    # embedding 层
+    embedding = tf.get_variable(name, [units_num, embedding_length])
+    return embedding
+
+def build_lstm(lstm_size, keep_prob, layers_num, name='lstm'):
+    # lstm层 build
+    def get_a_lstm(lstm_size, keep_prob):
+        lstm = tf.nn.rnn_cell.BasicLSTMCell(lstm_size)
+        drop = tf.nn.rnn_cell.DropoutWrapper(lstm, output_keep_prob=keep_prob)
+        return drop
+
+    with tf.name_scope(name):
+        cell = tf.nn.rnn_cell.MultiRNNCell(
+            [get_a_lstm(lstm_size, keep_prob) for i in range(layers_num)])
+    return cell
+
+def build_input(input_index, embedding_size=None, name='de'):
+    # 输入层 build
+    """
+    - embeddinng_size: ListType Length>=2
+    """
+    embedding_layer = build_embedding(embedding_size[0], embedding_size[1])
+    embedding_out = tf.nn.embedding_lookup(embedding_layer, input_index, name=name)
+    return embedding_out
+        
+def build_encoder():
+    # 编码层 build
+    
+    pass
+
+def build_decoder():
+    # 解吗层 build
+    pass
+
+def build_inference():
+    # 推理层 build
+    pass
+
+def corpus_util(corpus_data):
+    # 语料处理 one_hot
+    pass
+
+if __name__ == '__main__':
+    data = pd.read_csv('./cmn-eng/cmn.txt', sep='\t', names=['eng', 'chi'])
+    #最大英文单词数 32
+    #最大中文字符数 44
+    lstm_hidden_num = 100 # lstm 隐层维度
+    words_class = 120 # 单词的种类数
+    input_max_length = 15 # 最大输入长度
+    decoder_input_max_length = 19
+    batch_size = 10 #
+    fully_connect = 32
+    
+    # 建立input
+    inputs = tf.placeholder(tf.int32, shape=(None, input_max_length), name='inputs')
+    decoder_inputs = tf.placeholder(tf.int32, shape=(None, None), name='decoder_inputs')
+    targets = tf.placeholder(tf.int32, shape=(None, decoder_input_max_length), name='targets')
+    # 词嵌入层
+    embedding_layer = build_embedding(words_class, 100)
+    embedding_layer_chi = build_embedding(200, 100, name='chi')# 解码词语嵌入
+    # lstm 输入
+    lstm_inputs = tf.nn.embedding_lookup(embedding_layer, inputs)
+    # 编码层
+    encoder_layer = build_lstm(lstm_hidden_num, 0.9, 1)
+    # 初始化lstm 层
+    initial_state = encoder_layer.zero_state(batch_size, tf.float32)
+    # lstm 动态时间维度展开
+    encoder_lstm_outputs, encoder_final_state = tf.nn.dynamic_rnn(encoder_layer, lstm_inputs, dtype=tf.float32)
+    # 开始构建解码层
+    # 解码层第一个传入的字符为`<BOE>`
+    encoder_final_c = encoder_final_state[0][0] # 编码层最后的状态 c
+    # 解码层构建-for training
+    
+    decoder_lstm_inputs = tf.nn.embedding_lookup(embedding_layer_chi, decoder_inputs)
+    decoder_layer = build_lstm(lstm_hidden_num, 0.9, 1)
+    #decoder_layer = tf.contrib.rnn.LSTMCell(100)
+    decoder_lstm_outputs, decoder_final_state = tf.nn.dynamic_rnn(decoder_layer, decoder_lstm_inputs, initial_state=\
+                                                                  encoder_final_state, scope="plain_decoder")# 坑啊，两个dynamic 要用scope进行区分
+    # 构建损失函数
+    decoder_logits_argmax = tf.argmax(decoder_lstm_outputs, 2)
+    #decoder_logits = tf.to_float(decoder_logits, name='ToFloat32')
+    decoder_logits = tf.contrib.layers.linear(decoder_lstm_outputs, fully_connect)
+    cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(
+        labels=tf.one_hot(targets, depth=fully_connect, dtype=tf.float32),
+        logits=decoder_logits
+    )
+    loss = tf.reduce_mean(cross_entropy)
+    train_op = tf.train.AdamOptimizer().minimize(loss)
+    # 解码层构建-for inference
+    def inference_layer(inputs_infer):
+        """
+        推理层构建
+        - 结束条件
+          1. 达到最大字符数
+          2. 输出<END>
+        """
+        global dictionary_eng, dictionary_chi
+        global embeddinng_layer, embedding_layer_chi
+        global encoder_layer
+        global decoder_layer
+        #global encoder_final_state
+        stop_condition = False
+        count = 0
+        # 编码
+        inputs_encode = tf.nn.embedding_lookup(embedding_layer, inputs_infer)
+        encoder_lstm_outputs, encoder_final_state = tf.nn.dynamic_rnn(encoder_layer, inputs_encode, dtype=tf.float32)
+        # 推断初始输入
+        inference_initial_inputs = tf.nn.embedding_lookup(embedding_layer_chi, [[dictionary_chi[0]['bos']]])
+        string = ''
+        while not stop_condition:
+            # 预测字符
+            #判断是否终止
+            #需要向量转字符
+            #print(11)
+            inference_outputs, infenrence_state = tf.nn.dynamic_rnn(decoder_layer, inference_initial_inputs, \
+                                                                    initial_state=encoder_final_state,scope="plain_decoder")
+            print(np.argmax(inference_outputs))
+            outputs_str = dictionary_chi[1][np.argmax(inference_outputs)]
+            string += outputs_str
+            print(outputs_str, count)
+            if outputs_str == 'eos' or count > 20:
+                stop_condition = True
+            else:
+                inference_initial_inputs = inference_outputs
+                encoder_final_state = infenrence_state
+            count += 1
+            print(string)
+        return string
+
+
+    # begin train
+    # 训练参数定义
+    epochs = 100 # 训练次数
+    encoder_inputs, decoder_target, decoder_input = dataflow_loads()
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    saver = tf.train.Saver()
+    count = 0
+    for ep in range(epochs):
+        feed_dict = {inputs: encoder_inputs, decoder_inputs: decoder_input,
+                     targets: np.array(decoder_target),
+                     }
+        _, a, b = sess.run([train_op, loss, decoder_logits], feed_dict)
+        predict = sess.run(decoder_logits_argmax, feed_dict)
+        print(_, a, predict)
+        
+        print(count)
+        count += 1
+    a = predict
+    print(inference_layer([encoder_inputs[0]]))
+        
+    
+            
